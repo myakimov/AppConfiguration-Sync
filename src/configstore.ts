0296c4b1131e6e18936f9ff88f07ada6b445d151
Marin Yakimov <marin.yakimov@gmail.com>
2022-10-17 21:58:43 +0300

    Updated connection string logging

--- src/configstore.ts
+++ src/configstore.ts
@@ -24,7 +24,7 @@ export async function syncConfig(config: any, connectionString: string, strict: 
         }
     };
 
-    core.info(connectionString);
+    core.info("Connection String :" + connectionString);
 
     const client = new AppConfigurationClient(connectionString);
 
